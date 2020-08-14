fa6bd0c8ba0d5f2ebfd8b8cee5bd469fd3784d1d
kiru07 <kirushanthan007@gmail.com>
2020-08-14 12:37:54 +0530

    Merged Feature_AppRouting into master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewBlogComponent } from './view-blog/view-blog.component';
+import { CreateBlogComponent } from './create-blog/create-blog.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlogsComponent },
+  { path: 'create-blog', component: CreateBlogComponent },
+  { path: 'view-blog/:id', component: ViewBlogComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
